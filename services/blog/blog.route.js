540e409eedff0049e9dae28c2b206590a4096d10
Jose Pol Sastre <polsastre3@gmail.com>
2018-05-10 20:46:46 +0200

    middleware added

--- services/blog/blog.route.js
+++ services/blog/blog.route.js
@@ -4,14 +4,27 @@ const mongoose = require('mongoose');
 const express = require('express');
 
 const BlogService = require('./blog.service');
+const middleware = require('../middleware');
 
 const router = express.Router();
 
 const blogService = new BlogService();
 
-router.get('/all', (req, res, next) => {
+router.get('/all', middleware, (req, res) => {
     blogService.getBlogs().then(blogs => res.send(blogs))
     .catch(error => res.status(400).send(error));
+});
+
+router.get('/categories/all', middleware, (req, res) => {
+
+});
+
+router.get('recentsAndPopulars/all', middleware, (req, res) => {
+
+});
+
+router.get('tags/all', middleware, (req, res) => {
+
 });
 
 module.exports = router;
---
+++ services/middleware.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
+
+const APP_CONSTANTS = require('../app.constants');
+const UserService = require('./users/users.service');
+
+const userService = new UserService();
+
+const middleware = (req, res, next) => {
+    const authorization = req.headers.authorization;
+
+    if (!authorization) res.status(403).send({ message: "No token" });
+
+    const bearerToken = authorization.split(' ')[1];
+    const payload = jwt.decode(bearerToken, APP_CONSTANTS.TOKEN_EXPIRATION_TIME);
+
+    if (!payload || payload.exp <= moment().unix()) {
+        res.status('401').send({ message: !payload ? "Invalid token" : "Token expired" });
+        return;
+    }
+
+    userService.refresh(payload.id).then(user => next())
+    .catch(error => res.status('401').send(error));
+}
+
+module.exports = middleware;
